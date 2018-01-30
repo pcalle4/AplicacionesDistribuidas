8d9747ada476e2e7db2ac88eb98ae2da76c152a8
Paola Valverde <paoadri94@gmail.com>
2018-01-29 19:58:57 -0500

    PRUEBA

--- SoScholar/src/main/java/interciclo/controlador/logAdminControlador.java
+++
@@ -1,5 +0,0 @@
-package interciclo.controlador;
-
-public class logAdminControlador {
-
-}
--- SoScholar/src/main/java/interciclo/servicios/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package interciclo.servicios;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/srv")
-public class RestApplication extends Application  {
-
-}
